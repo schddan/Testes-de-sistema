6f268f503178bbf162c1cfd8f8321a304e8ed978
schddan <danielschmitzsc@gmail.com>
2024-09-04 12:31:26 -0300

    Início da tela de login para realização dos testes

--- End-to-end/script.js
+++ End-to-end/script.js
@@ -3,12 +3,24 @@ function validate(event) {
 
     const username = document.getElementById('username').value 
     const password = document.getElementById('password').value
+    const confirmPassword = document.getElementById('confirmPassword').value
 
     console.log(username)
 
-    if(username == 'admin' && password == 'admin') {
+    if(username == 'admin' && password == 'admin' && confirmPassword == 'admin') {
         alert('Login efetuado com sucesso')
+        document.getElementById('username').value = ''
+        document.getElementById('password').value = ''
+        document.getElementById('confirmPassword').value = ''
+        document.getElementById('mensagemErro').style.display = 'none'
+    } else if(username == '' || password == '' || confirmPassword == ''){
+        document.getElementById('mensagemErro').style.display = 'block'
+        document.getElementById("mensagemErro").innerHTML="É necessário preencher todos os campos";
+    } else if (password != confirmPassword){
+        document.getElementById('mensagemErro').style.display = 'block'
+        document.getElementById("mensagemErro").innerHTML="As senhas não correspondem";
     } else {
         document.getElementById('mensagemErro').style.display = 'block'
+        document.getElementById("mensagemErro").innerHTML="Dados incorretos";
     }
 }
